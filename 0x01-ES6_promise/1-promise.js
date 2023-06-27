ab4f04468e0d4599fdebaa2e77762a9ebbf5ca91
stephenchuks <stephenchuks@my.uopeople.edu>
2023-06-27 12:09:24 +0000

    hot fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,13 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
+  const myPromise = new Promise((resolve, reject) => {
     if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
+      resolve(
+        { status: 200, body: 'Success' },
+      );
     } else {
-      reject(new Error(
-        'The fake API is not working currently',
-      ));
+      reject(new Error('The fake API is not working currently'));
     }
   });
+  return myPromise;
 }
+
