b81edc8ece764d7d6b1ed2925e6f4f97d26c1f0d
stephenchuks <stephenchuks@my.uopeople.edu>
2023-06-27 11:46:43 +0000

    hot fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,8 @@
 export default function getResponseFromAPI() {
-  const myPromise = new Promise((resolve) => {
-    resolve();
-  });
+  const myPromise = new Promise(
+    (resolve) => {
+      resolve();
+    },
+  );
   return myPromise;
 }
-
